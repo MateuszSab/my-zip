2794b2f08b14ac0f9f8049ee966d29cbfea5f2e4
a_szczepanik <a.szczepanik@adform.com>
2022-05-20 13:31:48 +0200

    Testing empty lists

--- src/test/scala/ZipSequencesSpec.scala
+++ src/test/scala/ZipSequencesSpec.scala
@@ -1,36 +1,20 @@
+import org.scalacheck.Prop.forAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+import org.scalatest.prop.TableDrivenPropertyChecks.whenever
 import org.scalatestplus.scalacheck.Checkers
+
+import scala.List
 
 class ZipSequencesSpec extends AnyFlatSpec with should.Matchers with Checkers {
 
   import ZipSequences._
 
-  "zipper" should "check if lengths of two sequences are equal" in {
+  it should "check if lengths of two sequences are equal" in {
     check((a: List[Int], b: List[Int]) => zipper(a, b).size == (a ::: b).size)
   }
 
-  "zipper" should "intertwine two sequences" in {
-    val s1 = Seq(1, 2, 3)
-    val s2 = Seq('a', 'a', 'a')
-    val expected = Seq(1, 'a', 2, 'a', 3, 'a')
-
-    zipper(s1, s2) shouldBe expected
+  it should "return empty list" in {
+    check((a: List[Int], b: List[Int]) => zipper(a, b).empty == List())
   }
-
-  "zipper" should "intertwine two sequences of different size, first is shorter" in {
-    val s1 = Seq(1, 2)
-    val s2 = Seq('a', 'a', 'a')
-    val expected = Seq(1, 'a', 2, 'a', 'a')
-
-    zipper(s1, s2) shouldBe expected
-  }
-  "zipper" should "intertwine two sequences of different size" in {
-    val s1 = Seq(1, 2, 3)
-    val s2 = Seq('a', 'a')
-    val expected = Seq(1, 'a', 2, 'a', 3)
-
-    zipper(s1, s2) shouldBe expected
-  }
-
 }
