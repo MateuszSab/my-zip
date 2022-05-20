158aec60e69df0fc07be77ff431390bf18e9764c
m.sabatowski <ma.sabatowski@gmail.com>
2022-05-20 19:43:17 +0200

    git workflow added, function modified, all tests passing

--- src/main/scala/myZip.scala
+++ src/main/scala/myZip.scala
@@ -1,22 +1,16 @@
 object myZip {
   def myzip(s1: Seq[Any], s2: Seq[Any]): Seq[Any] = {
-    def x(first: Seq[Any], second: Seq[Any]): (Seq[Any], Seq[Any], Seq[Any]) = {
-      ???
+    if (s1.length == s2.length) s1.zip(s2).flatMap(_.productIterator)
+    else if (s1.isEmpty) s2
+    else if (s2.isEmpty) s1
+    else if (s1.isEmpty & s2.isEmpty) List.empty
+    else {
+      val longer = if (s1.length > s2.length) s1 else s2
+      val shorter = if (s1.length > s2.length) s2 else s1
+      val equal = longer.dropRight(longer.length - shorter.length)
+      val remainder = longer.takeRight(longer.length - shorter.length)
+      if (s1 == longer) equal.zip(shorter).flatMap(_.productIterator) ++ remainder
+      else shorter.zip(equal).flatMap(_.productIterator) ++ remainder
     }
-    if (s1.length > s2.length) {
-      val eq1 = s1.dropRight(s1.length - s2.length)
-      eq1.zip(s2).flatMap(_.productIterator) :+ s1.tail(s1.length - s2.length)
-    }
-    else if (s2.length > s1.length) {
-      val eq2 = s2.dropRight(s2.length - s1.length)
-      //      val x1 = eq2.zip(s1).flatMap{ case (a, b) => Seq(a,b) }
-      val x1 = s1.zip(eq2).flatMap(_.productIterator)
-      val x2 = s2.tail(s2.length - s1.length)
-      val x3 = x1 :+ x2
-      x3
-    }
-    else s1.zip(s2).flatMap(_.productIterator)
-    s1.zip(s2).flatMap(_.productIterator)
   }
-
 }
--- src/test/scala/myZipSpec.scala
+++ src/test/scala/myZipSpec.scala
@@ -1,9 +1,25 @@
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should
+import org.scalatest.matchers._
+import org.scalatest.prop.TableDrivenPropertyChecks.whenever
+import org.scalatestplus.scalacheck.Checkers
+import org.scalatestplus.scalacheck.Checkers._
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks.forAll
 
 class myZipSpec extends AnyFlatSpec with should.Matchers {
 
   import myZip._
+
+  "myzip" should "create a seq of size equals to sum of two seq" in {
+    check((a: List[Int], b: List[String]) => myzip(a, b).size == (a ::: b).size)
+  }
+
+  "myzip" should "return empty list if both input lists are empty" in {
+    forAll { (a: Seq[Int], b: Seq[String]) =>
+      whenever(a.nonEmpty && b.nonEmpty) {
+        myzip(a, b).take(2) shouldBe Seq(a.head, b.head)
+      }
+    }
+  }
 
   "myzip" should "intertwine two sequences" in {
     val s1 = Seq(1, 2, 3)
@@ -15,8 +31,8 @@ class myZipSpec extends AnyFlatSpec with should.Matchers {
 
   "myzip" should "intertwine two sequences of different size, first is shorter" in {
     val s1 = Seq(1, 2)
-    val s2 = Seq('a', 'a', 'a')
-    val expected = Seq(1, 'a', 2, 'a', 'a')
+    val s2 = Seq("a", "a", "a")
+    val expected = Seq(1, "a", 2, "a", "a")
 
     myzip(s1, s2) shouldBe expected
   }
